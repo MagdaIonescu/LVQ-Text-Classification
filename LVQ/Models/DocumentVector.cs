559f09b1d09858c97d9cb1e28d30f3e09849f8f2
Magda <mionescu719@gmail.com>
2026-03-28 13:12:32 +0200

    Add DocumentVector model

---
+++ LVQ/Models/DocumentVector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LVQ.Models
+{
+    public class DocumentVector
+    {
+        public double[] Features { get; set; }
+        public string Label { get; set; }
+
+        public DocumentVector(double[] features, string label)
+        {
+            Features = features;
+            Label = label;
+        }
+    }
+}
